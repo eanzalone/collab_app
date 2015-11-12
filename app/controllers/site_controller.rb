2592dba71d2ecc0bd44249fc525e5041176af24b
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-11-11 17:19:54 -0800

    added splash

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,6 @@
 class SiteController < ApplicationController
 	def index
 	end
+	def splash
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'site#index'
 
+  root to: 'site#splash'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
